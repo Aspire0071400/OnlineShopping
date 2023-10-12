86d0b84f1fdd815028654785d073d7eba61e558d
Aspire0071400 <prabhujivats@gmail.com>
2023-10-13 00:53:23 +0530

    update sdk from 33 to 34

--- app/src/main/java/com/aspire/onlineshopping/LandingPage.java
+++ app/src/main/java/com/aspire/onlineshopping/LandingPage.java
@@ -3,6 +3,8 @@ package com.aspire.onlineshopping;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingPage extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class LandingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
     }
 }
